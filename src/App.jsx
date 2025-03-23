cf9e85456132656b2ee11a6202eadcd7be55f858
Abdul Sami <45204596+abdulsami123@users.noreply.github.com>
2025-03-23 14:01:19 -0500

    React Router created

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import HomeCards from "../components/HomeCards";
-import JobListings from "../components/JobListings";
-import ThemeToggle from "../components/ThemeToggle";
+import MainLayout from "./layouts/MainLayout";
+import React from "react";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { RouterProvider , createBrowserRouter ,createRoutesFromElements,Route } from "react-router";
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<Home />} />
+      <Route path='/About' element={<About />} />
+    </Route>
+  )
+);
+
 
 const App = () => {
-  return (
-    <>
-    
-
-    <Navbar />
-    
-    <Hero />
-
-    <HomeCards />
-
-    <JobListings />
-
-    <ThemeToggle />
-      
-
-  
-          
-    </>
-  );
+  return  <RouterProvider router={router} />;
 };
 
 export default App;
